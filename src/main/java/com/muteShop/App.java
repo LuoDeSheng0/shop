ff2f847cefdbb10918a4160abec4e5276c5e164a
mute_luo@163.com <yhlb123456>
2019-03-07 17:27:41 +0800

    shop0307

---
+++ src/main/java/com/muteShop/App.java
@@ -0,0 +1,14 @@
+package com.muteShop;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
